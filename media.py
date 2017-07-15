263b377fa247261f406657f11c4e91ef722a72d2
Malik Albeik <abd.beik@me.com>
2017-07-15 19:26:38 +0300

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,12 @@
 class Movie():
     def __init__(self, title, movie_story, poster_image_url, trailer_youtube_url):
+        """ Initialize the movie instance.
+            Arguments:
+            title: title of the movie
+            movie_story: a short description of the movie.
+            poster_image_url:the movie's image url.
+            trailer_youtube_url:trailer's youtube video link.
+            Returns: None. """
         self.title = title
         self.storyline = movie_story
         self.poster_image_url = poster_image_url
